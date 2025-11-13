2d4ed04d01e5543363ccdd5f3af5d1acb72a4e21
JosefinaSilva96 <josefinasilvafuente@gmail.com>
2025-11-13 09:09:58 +0000

    changes

---
+++ Code/01-prueba-uss.R
@@ -0,0 +1,68 @@
+# Solemne Universidad San Sebastian
+
+
+### Cargar Librerias----
+
+library(haven)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(labelled)
+library(data.table)
+library(readxl)
+library(readr)
+library(purrr)
+library(haven)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(labelled)
+library(data.table)
+library(ggplot2)
+library(shinydashboard)
+library(shiny)
+library(shinythemes)
+library(DT)
+library(maps)
+library(mapdata)
+library(leaflet)
+library(rnaturalearth)
+library(sf)
+library(plotly)
+library(officer)
+library(flextable)
+library(viridis)
+library(here)
+library(glue)
+library(colourpicker)
+library(wbstats)
+library(htmlwidgets)
+library(bs4Dash)
+library(countrycode)
+library(bslib)
+library(lubridate)
+library(scales)
+library(patchwork)
+library(zoo)
+library(scales)
+library(purrr)
+library(forcats)
+library(readxl)
+library(stringi)  
+library(readxl)
+
+
+#Directorios---- AQUI CAMBIAR!  
+
+
+data_path_in <- "C:/Users/wb631166/OneDrive - WBG/Desktop/Prueba USS"
+
+data_path_out <- "C:/Users/wb631166/OneDrive - WBG/Desktop/Prueba USS/Outputs"
+
+#Cargar base de datos ----
+
+
+
+
+
+
